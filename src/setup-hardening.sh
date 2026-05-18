4b6dc5254b50012e138dffdac830f3444cf15a06
Monika Campoli <164352666+MCampoli@users.noreply.github.com>
2026-05-18 14:05:18 +0200

    Update setup-hardening.sh
    poprawka skryptu o port 2222

--- src/setup-hardening.sh
+++ src/setup-hardening.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# Sprint 2 – Automatyzacja hardeningu Debian
+#Automatyzacja hardeningu Debian
+# Uruchom jako root!
+
+# Sprawdzenie uprawnień
+if [ "$EUID" -ne 0 ]; then 
+    echo "Uruchom jako root (sudo ./skrypt.sh)"
+    exit 1
+fi
+
+# Backup plików konfiguracyjnych
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup 2>/dev/null
+cp /etc/sysctl.conf /etc/sysctl.conf.backup 2>/dev/null
 
 echo "=== UFW ==="
+apt update
 apt install ufw -y
 ufw default deny incoming
 ufw default allow outgoing
@@ -9,8 +21,8 @@ ufw allow 2222/tcp
 ufw enable
 
 echo "=== SSH Hardening ==="
-sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config
-sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
+sed -i 's/^#\?Port 22/Port 2222/' /etc/ssh/sshd_config
+sed -i 's/^#\?MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
 systemctl restart ssh
 
 echo "=== Fail2ban ==="
@@ -20,7 +32,7 @@ systemctl start fail2ban
 
 echo "=== Auditd / AIDE ==="
 apt install auditd aide -y
-aideinit
+aideinit -f
 
 echo "=== Kernel sysctl ==="
 cat << EOF >> /etc/sysctl.conf
@@ -30,4 +42,7 @@ kernel.dmesg_restrict = 1
 EOF
 sysctl -p
 
-echo "Hardening completed."
+echo "=== Hardening completed! ==="
+echo "SSH działa na porcie 2222"
+echo "UFW aktywny, Fail2ban uruchomiony"
+echo "AIDE baza danych zainicjalizowana"ho "=== UFW ==="
